dd1040ecc9034f47be9e65357e2f1b29a8bcc6b8
Samir Ghosh <hello@samir.tech>
2023-02-11 20:29:02 -0800

    add: eyes, mouth

--- src/script.js
+++ src/script.js
@@ -30,6 +30,8 @@ const parameters = {
     normalScale: 0.3,
     clearcoatNormalScale: 0.2,
     normalRepeat: 3,
+
+
     // attenuationTint: 0xffffff,
     // attenuationDistance: 0,
 }
@@ -67,6 +69,12 @@ const material = new THREE.MeshPhysicalMaterial({
     color:parameters.objcolor,
 })
 
+const eyeMaterial = new THREE.MeshBasicMaterial({
+    color: 'black',
+    side: THREE.DoubleSide
+})
+
+
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
 // gui.add(material,'').min(0).max(1).step(0.0001)
@@ -76,15 +84,75 @@ gui.addColor(parameters, 'objcolor').onChange(()=>
     material.color.set(parameters.objcolor)
 })
 
+const cuteThing = new THREE.Group()
+
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(.5,16,16),
     material
 )
+sphere.scale.y = 0.9
+
 sphere.geometry.setAttribute(
     'uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
     )
 
-scene.add(sphere)
+cuteThing.add(sphere)
+
+
+//in degrees
+const eyeSize = 0.03
+const eyeSpacing = Math.PI/15
+const forehead = Math.PI/8
+const eyeProtrusion = 0.05 //higher number goes more in
+
+const eyeGeometry = new THREE.SphereGeometry(eyeSize, 16, 16)
+const Leye = new THREE.Mesh(
+    eyeGeometry,
+    eyeMaterial
+)
+Leye.position.z = (.5 - eyeProtrusion) *Math.cos(eyeSpacing)
+Leye.position.x =  (.5 - eyeProtrusion) * Math.sin(eyeSpacing)
+Leye.position.y = (.5 - eyeProtrusion) * Math.sin(-forehead)
+cuteThing.add(Leye)
+const Reye = new THREE.Mesh(
+    eyeGeometry,
+    eyeMaterial
+)
+Reye.position.z = (.5 - eyeProtrusion) *Math.cos(- eyeSpacing)
+Reye.position.x =  (.5 - eyeProtrusion) * Math.sin(- eyeSpacing)
+Reye.position.y = (.5 - eyeProtrusion) * Math.sin(-forehead)
+cuteThing.add(Reye)
+
+const mouthGeometry = new THREE.TorusGeometry(
+    6,
+    .3,
+    16,
+    50,
+    1.87
+)
+
+
+const mouthPosition = 2 * Math.PI/15
+const mouth = new THREE.Mesh(
+    mouthGeometry,
+    eyeMaterial
+)
+mouth.rotation.x += Math.PI/3 + Math.PI - Math.PI/9
+mouth.rotation.z += Math.PI/5 
+const mouthScale = 0.03
+mouth.scale.set(mouthScale,mouthScale,mouthScale)
+mouth.position.set(
+    0,
+    (0.5 + 0.1 - 6 * mouthScale) * Math.sin(-mouthPosition),
+    (0.5 + 0.2 - 6 * mouthScale) * Math.cos(mouthPosition)
+)
+
+cuteThing.add(mouth)
+
+
+
+
+scene.add(cuteThing)
 
 /**
  * Lights
@@ -134,8 +202,9 @@ window.addEventListener('resize', () =>
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
-camera.position.y = 1
+camera.position.y = -.74
 camera.position.z = 2
+
 scene.add(camera)
 
 // Controls
